cbf7084bc62fb2c00fbc60f801b6cb2f97e1549e
roddigoz6 <131356598+roddigoz6@users.noreply.github.com>
2024-01-23 12:46:25 +0100

    1st comit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
